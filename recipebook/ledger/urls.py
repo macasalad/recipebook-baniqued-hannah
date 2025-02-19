d9a3f7531c8e144757a5b325a527f2d87bb60714
macasalad <hannahleibaniqued12@gmail.com>
2025-02-19 19:41:57 +0800

    Created Recipe 1 page

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import recipes
+from .views import recipes, recipe1
 
 urlpatterns = [
-    path('recipes/list/', recipes, name = "Recipe Book"),
+    path('recipes/list', recipes, name = "Recipe Book"),
+    path('recipe/1/', recipe1, name = "Recipe 1"),
 ]
 
 app_name = 'ledger'
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -67,3 +67,32 @@ def recipes(request):
     ]
 }
     return render(request, 'recipes.html', ctx)
+
+def recipe1(request):
+    ctx = {
+    "name": "Recipe 1",
+    "ingredients": [
+        {
+            "name": "tomato",
+            "quantity": "3pcs"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "pork",
+            "quantity": "1kg"
+        },
+        {
+            "name": "water",
+            "quantity": "1L"
+        },
+        {
+            "name": "sinigang mix",
+            "quantity": "1 packet"
+        }
+    ],
+    "link": "/recipe/1"
+}
+    return render(request, 'recipe.html', ctx)
