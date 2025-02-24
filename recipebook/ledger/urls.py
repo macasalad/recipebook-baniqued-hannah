bf7c72ca6dc2d2289d84d372bbaf2a3f16cdad33
macasalad <hannahleibaniqued12@gmail.com>
2025-02-24 22:24:27 +0800

    Fixed templates formatting

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import recipes, recipe1, recipe2
 
 urlpatterns = [
-    path('recipes/list', recipes, name = "Recipe Book"),
+    path('recipes/list/', recipes, name = "Recipe Book"),
     path('recipe/1/', recipe1, name = "Recipe 1"),
     path('recipe/2/', recipe2, name = "Recipe 2"),
 ]
