fde2b0ce8d760d074bab20c8cf264bc1b09f5834
macasalad <hannahleibaniqued12@gmail.com>
2025-03-06 17:17:45 +0800

    Fixed quantity field in RecipeIngredient and string representations for Ingredient and Recipe models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -5,7 +5,7 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return '{self.name}'
+        return {self.name}
     
     def get_absolute_url(self):
         return reverse('ledger:ingredient_detail', args=[self.pk])
@@ -14,12 +14,12 @@ class Recipe(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return '{self.name}'
+        return {self.name}
     
     def get_absolute_url(self):
         return reverse('ledger:recipe_detail', args=[self.pk])
     
 class RecipeIngredient(models.Model):
-    quantity = models.IntegerField()
+    quantity = models.CharField(max_length=50)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name = "recipe")
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name = "ingredients")
