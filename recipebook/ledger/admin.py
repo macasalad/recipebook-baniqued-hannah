b02474bd8210990c19eb290f3c6c7883e4bb98a1
macasalad <hannahleibaniqued12@gmail.com>
2025-03-07 19:14:36 +0800

    Added admin page

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+    extra = 0
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+    list_display = ('name',)
+    list_filter = ('name',)
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+    list_display = ('name',)
+    list_filter = ('name',)
+    inlines = [RecipeIngredientInline]
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+    list_display = ('recipe', 'ingredient', 'quantity',)
+    list_filter = ('recipe', 'ingredient',)
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
