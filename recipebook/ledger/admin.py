f6b48653233bd2435a7530eb400a8618e54380cb
macasalad <hannahleibaniqued12@gmail.com>
2025-03-07 21:26:11 +0800

    Added comments for admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from .models import Ingredient, Recipe, RecipeIngredient
 
 class RecipeIngredientInline(admin.TabularInline):
+    '''
+    Inline admin for RecipeIngredient
+    '''
     model = RecipeIngredient
 
     extra = 0
 
 class IngredientAdmin(admin.ModelAdmin):
+    '''
+    Admin configuration for Ingredient model
+    '''
     model = Ingredient
 
     list_display = ('name',)
@@ -14,6 +20,9 @@ class IngredientAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 class RecipeAdmin(admin.ModelAdmin):
+    '''
+    Admin configuration for Recipe model
+    '''
     model = Recipe
 
     list_display = ('name',)
@@ -22,12 +31,16 @@ class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline]
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
+    '''
+    Admin configuration for RecipeIngredient model
+    '''
     model = RecipeIngredient
 
     list_display = ('recipe', 'ingredient', 'quantity',)
     list_filter = ('recipe', 'ingredient',)
     search_fields = ('name',)
 
+# Register models with their corresponding admin configurations
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
