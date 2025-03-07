d27344f1a60d87dcc57825758a889ef0af733803
macasalad <hannahleibaniqued12@gmail.com>
2025-03-07 21:27:45 +0800

    Added comments and fixed formatting for views.py

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render
 from .models import Recipe
 
 def recipes(request):
+    '''
+    View to list all recipes.
+    '''
     recipes = Recipe.objects.all()
     ctx = {
-        "recipes" : recipes
+        "recipes":recipes
     }
+
     return render(request, "recipes.html", ctx)
 
 def recipe(request, pk):
+    '''
+    View to display a single recipe and its ingredients.
+    '''
     recipe = Recipe.objects.get(pk = pk)
     ingredients = recipe.ingredients.all()
     ctx = {
-        "recipe": recipe,
-        "ingredients" : ingredients
+        "recipe":recipe,
+        "ingredients":ingredients
     }
+
     return render(request, "recipe.html", ctx)
