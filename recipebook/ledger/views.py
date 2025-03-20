b183345a73be270a54ac2954032dbf0b9a0b1f0a
macasalad <hannahleibaniqued12@gmail.com>
2025-03-21 00:10:51 +0800

    Required login to view recipe_list page

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from .models import Recipe
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def recipes(request):
     '''
     View to list all recipes.
     '''
     recipes = Recipe.objects.all()
     ctx = {
-        "recipes":recipes
+        "recipes": recipes
     }
 
     return render(request, "recipes.html", ctx)
