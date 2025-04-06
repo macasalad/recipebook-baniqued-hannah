e36946ecffc8f1d4c895dcb1ea977fc6d6de9511
macasalad <hannahleibaniqued12@gmail.com>
2025-04-06 22:15:47 +0800

    Fixed views.py

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -9,7 +9,7 @@ def recipe_list(request):
     View to list all recipes.
     '''
     recipes = Recipe.objects.all()
-    
+
     ctx = {
         "recipes": recipes
     }
@@ -33,80 +33,42 @@ def recipe_detail(request, pk):
 
 @login_required
 def create_recipe(request):
+    # Form for new recipe
+    recipe_form = RecipeForm()
     if request.method == 'POST':
         recipe_form = RecipeForm(request.POST)
-        recipe_ingredient_form = RecipeIngredientForm(request.POST)
-        
-        if recipe_form.is_valid() and recipe_ingredient_form.is_valid():
+        if recipe_form.is_valid():
             recipe = recipe_form.save(commit=False)
             recipe.author = request.user.profile
             recipe.save()
-            
-            recipe_ingredient = recipe_ingredient_form.save(commit=False)
-            recipe_ingredient.recipe = recipe
-            recipe_ingredient.save()
-            
             return redirect('ledger:create_recipe')
-    else:
-        recipe_form = RecipeForm()
-        recipe_ingredient_form = RecipeIngredientForm()
     
+    # Form for new recipe ingredient
+    recipe_ingredient_form = RecipeIngredientForm()
+    if request.method == 'POST':
+        recipe_ingredient_form = RecipeIngredientForm(request.POST)
+        if recipe_ingredient_form.is_valid():
+            recipe_ingredient = recipe_ingredient_form.save(commit=False)
+            recipe_ingredient.save()
+            return redirect('ledger:create_recipe')
+            
     ctx = {
         "recipe_form": recipe_form,
         "recipe_ingredient_form": recipe_ingredient_form
     }
     return render(request, "new_recipe.html", ctx)
-# def create_recipe(request):
-#     # Form for new recipe
-#     recipe_form = RecipeForm()
-#     if request.method == 'POST':
-#         recipe_form = RecipeForm(request.POST)
-#         if recipe_form.is_valid():
-#             recipe = recipe_form.save(commit=False)
-#             recipe.author = request.user.profile
-#             recipe.save()
-#             return redirect('ledger:create_recipe')
-    
-#     # Form for new recipe ingredient
-#     recipe_ingredient_form = RecipeIngredientForm()
-#     if request.method == 'POST':
-#         recipe_ingredient_form = RecipeIngredientForm(request.POST)
-#         if recipe_ingredient_form.is_valid():
-#             recipe_ingredient = recipe_ingredient_form.save(commit=False)
-#             recipe_ingredient.save()
-#             return redirect('ledger:create_recipe')
-    
-#     ctx = {
-#         "recipe_form": recipe_form,
-#         "recipe_ingredient_form": recipe_ingredient_form
-#     }
-#     return render(request, "new_recipe.html", ctx)
 
 @login_required
 def create_ingredient(request):
+    # Form for new ingredient
+    ingredient_form = IngredientForm()
     if request.method == 'POST':
         ingredient_form = IngredientForm(request.POST)
         if ingredient_form.is_valid():
             ingredient_form.save()
             return redirect('ledger:create_recipe')
-    else:
-        ingredient_form = IngredientForm()
     
     ctx = {
         "ingredient_form": ingredient_form
     }
     return render(request, "new_ingredient.html", ctx)
-# def create_ingredient(request):
-#     # Form for new ingredient
-#     ingredient_form = IngredientForm()
-#     if request.method == 'POST':
-#         ingredient_form = IngredientForm(request.POST)
-#         if ingredient_form.is_valid():
-#             ingredient = ingredient_form.save(commit=False)
-#             ingredient.save()
-#             return redirect('ledger:create_recipe')
-#     ctx = {
-#         "ingredient_form": ingredient_form
-#     }
-    
-#     return render(request, "new_ingredient.html", ctx)
