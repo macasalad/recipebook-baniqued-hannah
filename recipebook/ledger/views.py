a3f4f56e473c97d5e0db6f7c7f59416b6001be1a
macasalad <hannahleibaniqued12@gmail.com>
2025-04-06 18:48:21 +0800

    Added view for adding just a new recipe

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Recipe
+from .forms import RecipeForm
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -29,3 +30,19 @@ def recipe(request, pk):
     }
 
     return render(request, "recipe.html", ctx)
+
+@login_required
+def add_recipe(request):
+    recipe_form = RecipeForm()
+    if request.method == 'POST':
+        recipe_form = RecipeForm(request.POST)
+        if recipe_form.is_valid():
+            recipe = recipe_form.save(commit=False)
+            recipe.author = request.user.profile
+            recipe.save()
+            return redirect('ledger:recipe_detail', pk=recipe.pk)
+
+    ctx = {
+        "recipe_form": recipe_form
+    }
+    return render(request, "add_recipe.html", ctx)
