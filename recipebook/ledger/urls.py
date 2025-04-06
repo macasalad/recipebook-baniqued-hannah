d97d50edfa4b4652eddbb23881450d5a9620ea8d
macasalad <hannahleibaniqued12@gmail.com>
2025-04-06 22:17:47 +0800

    Fixed imported views and added paths for adding new recipes and ingredients

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import recipes, recipe
+from .views import recipe_list, recipe_detail, create_recipe, create_ingredient
 
 urlpatterns = [
-    path('recipes/list/', recipes, name="recipe_list"),
-    path('recipe/<int:pk>/', recipe, name="recipe_detail")
+    path('recipes/list/', recipe_list, name="recipe_list"),
+    path('recipe/<int:pk>/', recipe_detail, name="recipe_detail"),
+    path('recipe/add/', create_recipe, name="create_recipe"),
+    path('recipe/add/new-ingredient', create_ingredient, name="create_ingredient")
 ]
 
 app_name = 'ledger'
