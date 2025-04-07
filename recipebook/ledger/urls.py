22428fdc84c427157fce74dc1f2362146dc0cee5
macasalad <hannahleibaniqued12@gmail.com>
2025-04-07 22:43:14 +0800

    Added path for adding images to recipes

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import recipe_list, recipe_detail, create_recipe, create_ingredient, add_image
+from .views import recipe_list, recipe_detail, create_recipe, recipe_add_image
+
 
 urlpatterns = [
     path('recipes/list/', recipe_list, name="recipe_list"),
     path('recipe/<int:pk>/', recipe_detail, name="recipe_detail"),
     path('recipe/add/', create_recipe, name="create_recipe"),
-    path('recipe/add/new-ingredient/', create_ingredient, name="create_ingredient"),
-    path('recipe/<int:pk>/add_image/', add_image, name="add_image")
+    path('recipe/<int:pk>/add_image/', recipe_add_image, name="recipe_add_image")
 ]
 
 app_name = 'ledger'
